8b4b99be1886d9e2cfc6675a4e61a63d081a03f0
John Llombart <johnllombart@gmail.com>
2015-07-03 16:29:06 -0400

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pageMonitorWebApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the pageMonitorWebApp
+ */
+angular.module('pageMonitorWebApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pageMonitorWebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pageMonitorWebApp
+ */
+angular.module('pageMonitorWebApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
